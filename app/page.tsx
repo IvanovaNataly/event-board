9fda7bcfcde67383e55e0b0a1397ca56eea9b1db
IvanovaNataly <letter4natalia@gmail.com>
2025-08-24 22:24:02 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
       <h1>event-board</h1>
       <h2>This is a subtitle</h2>
       <h3>This is a sub-subtitle</h3>
+      <h2>This is another subtitle</h2>
     </>
   );
 }
